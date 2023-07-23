860eb21c91b88e7435b9b54df9fde0a4ea50104a
kento980037 <kento_0037@yahoo.co.jp>
2023-07-23 20:25:01 +0900

    add python command

--- go/main.go
+++ go/main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"C"
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"os/exec"
 )
 
 func main() {
-
 	e := echo.New()
-	e.GET("/", sayhelloName)
+	e.GET("/", callScrapingEndpoint)
 	e.Logger.Fatal(e.Start(":9090"))
 }
 
-func sayhelloName(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+func callScrapingEndpoint(c echo.Context) error {
+	output, err := exec.Command("cd ..", "&&", "python", "python/scraping.py").Output()
+	if err != nil {
+		return err
+	}
+	return c.String(http.StatusOK, string(output))
 }
